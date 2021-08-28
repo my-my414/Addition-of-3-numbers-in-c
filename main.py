f7d17f7968589a16872ef3090bbc56f415111501
Akshaya H <81792372+my-my414@users.noreply.github.com>
2021-08-28 18:26:09 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int c;
+    a=7;
+    b=6;
+    c=9;
+    printf("%d\n",a+b+c);//%d is a format specifier(place holder and takes integer type value in a memory)
+    return 0;
+}
